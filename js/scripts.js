116dc3df1aeecd39e8aeb55e26047c5560855d77
David Boedigheimer <stealth259@gmail.com>
2019-10-29 14:03:56 -0700

    Add second form and second function for dislpay.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 $(document).ready(function() {
   $("form#trans_survey").submit(function(e) {
     e.preventDefault();
+
     $("#work_result").show();
     $("input:checkbox[name=work-trans]:checked").each(function() {
       var workTransportMode = $(this).val();
@@ -11,6 +12,12 @@ $(document).ready(function() {
       $("#work_result").append(capitalWorkTransportMode + "<br>");
     });
 
+    $("#fun_result").show();
+    $("input:checkbox[name=fun-trans]:checked").each(function() {
+      var funTransportMode = $(this).val();
+      var capitalFunTransportMode = funTransportMode.toUpperCase();
+      $("#fun_result").append(capitalFunTransportMode + "<br>");
+    });
 
     $("#trans_survey").hide();
   });
